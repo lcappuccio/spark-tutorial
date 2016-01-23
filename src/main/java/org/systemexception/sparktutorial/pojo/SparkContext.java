e7a6a8c4af4c582b84749cff4c7ceb3954543726
Leonardo Cappuccio <leo@systemexception.org>
2016-01-23 23:31:19 +0100

    word count test

--- src/main/java/org/systemexception/sparktutorial/pojo/SparkContext.java
+++ src/main/java/org/systemexception/sparktutorial/pojo/SparkContext.java
@@ -1,7 +1,12 @@
 package org.systemexception.sparktutorial.pojo;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 /**
  * @author leo
@@ -19,5 +24,19 @@ public class SparkContext {
 
 	public JavaSparkContext getSparkContext() {
 		return sparkContext;
+	}
+
+	public void processFile(String fileName, String outputFolder) {
+		// Load the input data, which is a text file read from the command line
+		JavaRDD<String> input = sparkContext.textFile(fileName);
+
+		// Java 8 with lambdas: split the input string into words
+		JavaRDD<String> words = input.flatMap(s -> Arrays.asList( s.split( " " ) ) );
+
+		// Java 8 with lambdas: transform the collection of words into pairs (word and 1) and then count them
+		JavaPairRDD<String, Integer> counts = words.mapToPair(t -> new Tuple2( t, 1 ) ).reduceByKey( (x, y) -> (int)x + (int)y );
+
+		// Save the word count back out to a text file, causing evaluation.
+		counts.saveAsTextFile(outputFolder);
 	}
 }
