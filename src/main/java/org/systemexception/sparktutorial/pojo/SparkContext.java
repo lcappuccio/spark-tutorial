6a383f6949dd4db54226d9d182f77f8e8651b6fb
Leonardo Cappuccio <leo@systemexception.org>
2016-01-24 21:03:04 +0100

    readability

--- src/main/java/org/systemexception/sparktutorial/pojo/SparkContext.java
+++ src/main/java/org/systemexception/sparktutorial/pojo/SparkContext.java
@@ -38,8 +38,8 @@ public class SparkContext {
 		JavaRDD<String> words = input.flatMap(s -> Arrays.asList(s.split(" ")));
 
 		// Java 8 with lambdas: transform the collection of words into pairs (word and 1) and then count them
-		JavaPairRDD<String, Integer> counts = words.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) ->
-				(int) x + (int) y);
+		JavaPairRDD<String, Integer> counts = words.mapToPair(t -> new Tuple2(t, 1))
+				.reduceByKey((x, y) -> (int) x + (int) y);
 
 		// Save the word count back out to a text file, causing evaluation.
 		counts.saveAsTextFile(outputFolder);
@@ -60,8 +60,8 @@ public class SparkContext {
 		JavaRDD<String> chars = sparkContext.parallelize(stringBuffer);
 
 		// Java 8 with lambdas: transform the collection of chars into pairs (word and 1) and then count them
-		JavaPairRDD<String, Integer> counts = chars.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) ->
-				(int) x + (int) y);
+		JavaPairRDD<String, Integer> counts = chars.mapToPair(t -> new Tuple2(t, 1))
+				.reduceByKey((x, y) -> (int) x + (int) y);
 
 		// Save the word count back out to a text file, causing evaluation.
 		counts.saveAsTextFile(outputFolder);
