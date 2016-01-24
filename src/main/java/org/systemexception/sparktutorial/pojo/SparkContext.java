3ce0d07cc085e1fcbd5f0d549c8fbe1919fc7404
Leonardo Cappuccio <leo@systemexception.org>
2016-01-24 17:07:54 +0100

    spark log configurator to external file

--- src/main/java/org/systemexception/sparktutorial/pojo/SparkContext.java
+++ src/main/java/org/systemexception/sparktutorial/pojo/SparkContext.java
@@ -1,10 +1,10 @@
 package org.systemexception.sparktutorial.pojo;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.slf4j.Logger;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -15,14 +15,12 @@ import java.util.Arrays;
  */
 public class SparkContext {
 
-	private final SparkConf sparkConf;
 	private final JavaSparkContext sparkContext;
-	private final Logger logger;
 
 	public SparkContext() {
-		sparkConf = new SparkConf().setAppName("org.systemexception.sparktutorial").setMaster("local");
+		SparkConf sparkConf = new SparkConf().setAppName("org.systemexception.sparktutorial").setMaster("local");
+		PropertyConfigurator.configure(SparkContext.class.getClassLoader().getResource("spark_log4j.properties"));
 		sparkContext = new JavaSparkContext(sparkConf);
-		logger = sparkConf.log();
 	}
 
 	public JavaSparkContext getSparkContext() {
@@ -30,18 +28,17 @@ public class SparkContext {
 	}
 
 	public void processFile(String fileName, String outputFolder) {
-		logger.info("Start processing...");
 		// Load the input data, which is a text file read from the command line
 		JavaRDD<String> input = sparkContext.textFile(fileName);
 
 		// Java 8 with lambdas: split the input string into words
-		JavaRDD<String> words = input.flatMap(s -> Arrays.asList( s.split( " " ) ) );
+		JavaRDD<String> words = input.flatMap(s -> Arrays.asList(s.split(" ")));
 
 		// Java 8 with lambdas: transform the collection of words into pairs (word and 1) and then count them
-		JavaPairRDD<String, Integer> counts = words.mapToPair(t -> new Tuple2( t, 1 ) ).reduceByKey( (x, y) -> (int)x + (int)y );
+		JavaPairRDD<String, Integer> counts = words.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) ->
+				(int) x + (int) y);
 
 		// Save the word count back out to a text file, causing evaluation.
 		counts.saveAsTextFile(outputFolder);
-		logger.info("End processing");
 	}
 }
