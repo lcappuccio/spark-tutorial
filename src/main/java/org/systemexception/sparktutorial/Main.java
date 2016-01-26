b2db18db6c71c3bbea7c0ce454ac5e8c8babad74
Leonardo Cappuccio <leo@systemexception.org>
2016-01-26 10:46:17 +0100

    application logging

--- src/main/java/org/systemexception/sparktutorial/Main.java
+++ src/main/java/org/systemexception/sparktutorial/Main.java
@@ -2,6 +2,8 @@ package org.systemexception.sparktutorial;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.systemexception.sparktutorial.pojo.SparkContext;
 
 import java.util.Arrays;
@@ -12,23 +14,29 @@ import java.util.Arrays;
  */
 public class Main {
 
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
+		logger.info("Create Spark Context");
 		SparkContext sparkContext = new SparkContext();
 		JavaSparkContext sc = sparkContext.getSparkContext();
 
+		logger.info("Parallelize list of integers");
 		JavaRDD<Integer> ints = sc.parallelize(Arrays.asList(1,2,3,4));
 		JavaRDD<Integer> result = ints.map(x -> x * x);
 		for (Integer integer: result.collect()) {
-			System.out.println(integer);
+			logger.info("Item: " + integer);
 		}
 
+		logger.info("Parallelize Strings");
 		JavaRDD<String> strings = sc.parallelize(Arrays.asList("Hello World", "Goodbye World"));
 		JavaRDD<String> words = strings.flatMap(x -> Arrays.asList(x.split(" ")));
 		for (String word: words.collect()) {
-			System.out.println(word);
+			logger.info("Words: " + word);
 		}
 
+		logger.info("Reduce Integers");
 		Integer reduced = ints.reduce((x, y) -> x + y);
-		System.out.println(reduced);
+		logger.info("Reduced: " + reduced);
 	}
 }
